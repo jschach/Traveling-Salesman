aa4aae561532a9fd93410a922b77784c2ecebe80
jschach <schachnerje@appstate.edu>
2015-02-05 14:57:39 -0500

    get_parents method and select_parent method implemented

--- TravelingSalesmanProblem/Population.py
+++ TravelingSalesmanProblem/Population.py
@@ -1,5 +1,6 @@
 import Member.py
 import Cities.pv
+import random
 
 __author__ = 'Jenna'
 
@@ -14,11 +15,34 @@ class Population:
         self.population = []
         self.start_member = Member(15)
         self.population.append(self.start_member)
+        self.POPULATION_SIZE = 199
+        self.population_ranking = []
 
     # fills in the rest of the population array
     # with many random members for starting population
     def start_population(self):
+        for x in range(0, self.POPULATION_SIZE):
+            self.population.append\
+                (self.start_member.new_permutation())
 
+    # selects a random parent with the probability of choosing
+    # a better parent over a worse parent (1*n/sum(POPULATION_SIZE)
+    # where n = POPULATION_SIZE-- starting from the best ranked parent
+    def select_parent(self):
+        num1 = random.randint(0, self.POPULATION_SIZE - 1)
+        num2 = random.randint(0, self.POPULATION_SIZE)
+        # selects the lower ranked parent
+        if num1 <= num2:
+            return num1
+        else:
+            return num2
 
+    # selects the parents from the select_parent method
+    def get_parents(self):
+        mother_index = self.population.select_parent()
+        father_index = self.population.select_parent()
+        while mother_index <= father_index:
+            mother_index = self.population.select_parent()
 
-    def select_parents(self):
+    def get_population_ranking(self):
+        return 0
